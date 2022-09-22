8d7a437eafafc3d64194e98ea3b73a78703e6996
Kevin Stradtman <webkit.development.systems@gmail.com>
2022-09-22 17:05:33 -0400

    Initial commit

---
+++ src/main/java/com/stradtkt/beerworks/BeerWorksApplication.java
@@ -0,0 +1,13 @@
+package com.stradtkt.beerworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerWorksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerWorksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stradtkt/beerworks/BeerWorksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stradtkt.beerworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerWorksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
